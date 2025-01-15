4e29bd7821988d925b7c66c02dff1162bc01c2b9
Shailesh4508 <shailesh4508singh@gmail.com>
2025-01-15 19:37:02 +0530

    11th Jan 2025

---
+++ src/ex_02_Java_Basics/Lab017_printF.java
@@ -0,0 +1,15 @@
+package ex_02_Java_Basics;
+
+public class Lab017_printF {
+    public static void main(String[] args) {
+        int a = 21;
+        System.out.printf("Your variable name is %d", a);
+//        %d -> int, byte, long, short  ----data
+//        %s
+//        %f float, %b boolean
+
+        byte b = 10;
+        System.out.println();
+        System.out.printf("%d", b);
+    }
+}
---
+++ src/ex_02_Java_Basics/Lab018_Practice_9xTable.java
@@ -0,0 +1,30 @@
+package ex_02_Java_Basics;
+
+public class Lab018_Practice_9xTable {
+
+    public static void main(String[] args) {
+
+    int s = 9;
+
+        System.out.printf("9*1=%d",s*1);
+        System.out.println();
+        System.out.printf("9*2=%d",s*2);
+        System.out.println();
+        System.out.printf("9*3=%d",s*3);
+        System.out.println();
+        System.out.printf("9*4=%d",s*4);
+        System.out.println();
+        System.out.printf("9*5=%d",s*5);
+        System.out.println();
+        System.out.printf("9*6=%d",s*6);
+        System.out.println();
+        System.out.printf("9*7=%d",s*7);
+        System.out.println();
+        System.out.printf("9*8=%d",s*8);
+        System.out.println();
+        System.out.printf("9*9=%d",s*9);
+        System.out.println();
+        System.out.printf("9*10=%d",s*10);
+}
+}
+
