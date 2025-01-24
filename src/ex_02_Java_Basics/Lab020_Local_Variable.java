ec38213ffa5afd02ae02e18242fa0582b4b37f5c
Shailesh4508 <shailesh4508singh@gmail.com>
2025-01-24 14:52:26 +0530

    24th Jan 2025

---
+++ src/ex_02_Java_Basics/Lab020_Local_Variable.java
@@ -0,0 +1,16 @@
+package ex_02_Java_Basics;
+
+public class Lab020_Local_Variable {
+    public static void main(String[] args) {
+        int a = 10;
+        byte b =10;
+        short s = 10;
+        long l = 9876448898L;
+        float f = 3.14f;
+        double d =45.874387487987;
+        boolean b1 = true;
+        char c = 'A';
+//        char c1 = "A"; not possible as double quotes will become string
+        System.out.println(l);
+    }
+}
---
+++ src/ex_02_Java_Basics/Lab021_Multiple_Variable.java
@@ -0,0 +1,11 @@
+package ex_02_Java_Basics;
+
+public class Lab021_Multiple_Variable {
+    public static void main(String[] args) {
+        int a = 10, b = 23, c = 25;
+
+        System.out.println(c);
+
+//        int r = 20, int b = 30 not possible
+    }
+}
