95568298bcd657fc8434849f8745fbf6677cd20b
Shailesh4508 <shailesh4508singh@gmail.com>
2025-02-09 02:29:52 +0530

    8 Feb 2025

--- src/TASK_PRACTISE_CODE_25_Jan/P_003_Task3_Palindrome.java
+++ src/TASK_PRACTISE_CODE_25_Jan/P_003_Task3_Palindrome.java
@@ -5,5 +5,23 @@ import java.util.Scanner;
 public class P_003_Task3_Palindrome {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your Words: ");
+        String str = sc.next();
+
+        String reversedStr = new StringBuilder(str).reverse().toString();
+
+        if (str.equalsIgnoreCase(reversedStr)){
+            System.out.println("The word is a Palindrome");
+        }
+        else {
+            System.out.println("The word is not a Palindrome");
+        }
+
+
     }
 }
+
+
+
+
