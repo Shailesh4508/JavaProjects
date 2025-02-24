50de22d6a8064c58e51c1719e7c56a70cac6a906
Shailesh4508 <shailesh4508singh@gmail.com>
2025-02-24 15:04:07 +0530

    12 Feb 2025

--- src/TASK_PRACTISE_CODE_9_Feb/P_002_Second_Smallest_Number.java
+++ src/TASK_PRACTISE_CODE_9_Feb/P_002_Second_Smallest_Number.java
@@ -1,5 +1,12 @@
 package TASK_PRACTISE_CODE_9_Feb;
 
-public class P_002_Second_Smallest_Number {
+import java.util.Scanner;
 
+public class P_002_Second_Smallest_Number {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the length of Arrays: ");
+        int num = sc.nextInt();
+    }
 }
